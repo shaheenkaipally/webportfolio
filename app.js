4d8ec9e15f8c50aba1bbe835aa9fb9e4e7ac0861
Shaheen Kaipally <77917105+shaheenkaipally@users.noreply.github.com>
2022-12-29 15:06:52 -0500

    Update app.js

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ let indexRouter = require('./routes/index');
 let usersRouter = require('./routes/users');
 
 let app = express();
-app.listen(80, function() {
-    console.log("Server is running on port " + 80);
+app.listen(3000, function() {
+    console.log("Server is running on port " + 3000);
 });
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
